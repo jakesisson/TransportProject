8e19d2c5eb3ffed1c943544aff21128b65cb7b7b
jakesisson <139182470+jakesisson@users.noreply.github.com>
2023-10-09 12:46:36 -0400

    Update AirTransport.java

--- AirTransport.java
+++ AirTransport.java
@@ -1,5 +1,9 @@
 public class AirTransport extends Transport{
-    public AirTransport(String mode, double price, boolean isRental, boolean isTicket, String pickup, int maxPassengers, double avgSpeed) {
-        super(mode, price, isRental, isTicket, pickup, maxPassengers, avgSpeed);
+    protected boolean isRecreational;
+    protected boolean isLongDistance;
+    public AirTransport(boolean isRecreational, boolean isLongDistance, String name, String mode, double price, boolean isRental, boolean isTicket, String pickup, int maxPassengers, double avgSpeed) {
+        super(name, mode, price, isRental, isTicket, pickup, maxPassengers, avgSpeed);
+        this.isRecreational = isRecreational;
+        this.isLongDistance = isLongDistance;
     }
 }
