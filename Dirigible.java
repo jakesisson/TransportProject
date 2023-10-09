968863b13ec030f91dac36e07fd8d397f570fdaa
jakesisson <139182470+jakesisson@users.noreply.github.com>
2023-10-09 12:50:18 -0400

    Update Dirigible.java

--- Dirigible.java
+++ Dirigible.java
@@ -1,7 +1,7 @@
 public class Dirigible extends AirTransport{
-    protected String name;
+    
     public Dirigible(String name, String mode, double price, boolean isRental, boolean isTicket, String pickup, int maxPassengers, double avgSpeed) {
-        super(mode, price, isRental, isTicket, pickup, maxPassengers, avgSpeed);
-        this.name = name;
+        super(isRecreational, isLongDistance, name, mode, price, isRental, isTicket, pickup, maxPassengers, avgSpeed);
+        
     }
 }
