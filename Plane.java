c1a95f84e12aa2109c20bbbeee34e5d2a448db7f
jakesisson <139182470+jakesisson@users.noreply.github.com>
2023-10-22 12:26:32 -0400

    Delete Plane.java

--- Plane.java
+++
@@ -1,7 +0,0 @@
-public class Plane extends AirTransport{
-    protected String name;
-    public Plane(String name, String mode, double price, boolean isRental, boolean isTicket, String pickup, int maxPassengers, double avgSpeed) {
-        super(mode, price, isRental, isTicket, pickup, maxPassengers, avgSpeed);
-        this.name = name;
-    }
-}
