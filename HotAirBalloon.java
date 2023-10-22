7ce558e9febaa6af4fb01389bb6ab08804116ab1
jakesisson <139182470+jakesisson@users.noreply.github.com>
2023-10-22 12:26:13 -0400

    Delete HotAirBalloon.java

--- HotAirBalloon.java
+++
@@ -1,7 +0,0 @@
-public class HotAirBalloon extends AirTransport{
-    protected String name;
-    public HotAirBalloon(String name, String mode, double price, boolean isRental, boolean isTicket, String pickup, int maxPassengers, double avgSpeed) {
-        super(mode, price, isRental, isTicket, pickup, maxPassengers, avgSpeed);
-        this.name = name;
-    }
-}
