69bc1738442a2c52699374e65482a95db1945b80
jakesisson <139182470+jakesisson@users.noreply.github.com>
2023-10-22 12:27:09 -0400

    Delete WaterTransport.java

--- WaterTransport.java
+++
@@ -1,5 +0,0 @@
-public class WaterTransport extends Transport{
-    public WaterTransport(String mode, double price, boolean isRental, boolean isTicket, String pickup, int maxPassengers, double avgSpeed) {
-        super(mode, price, isRental, isTicket, pickup, maxPassengers, avgSpeed);
-    }
-}
