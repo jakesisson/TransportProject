80bd1729e8b4a9ffe79c137d9115044498f8156d
jakesisson <139182470+jakesisson@users.noreply.github.com>
2023-10-22 12:26:50 -0400

    Delete Submarine.java

--- Submarine.java
+++
@@ -1,7 +0,0 @@
-public class Submarine extends WaterTransport{
-    protected String name;
-    public Submarine(String name, String mode, double price, boolean isRental, boolean isTicket, String pickup, int maxPassengers, double avgSpeed) {
-        super(mode, price, isRental, isTicket, pickup, maxPassengers, avgSpeed);
-        this.name = name;
-    }
-}
