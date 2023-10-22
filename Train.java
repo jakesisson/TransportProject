badced0e6051a6eb6a1ddf95b5e67f5c02a9a281
jakesisson <139182470+jakesisson@users.noreply.github.com>
2023-10-22 12:26:59 -0400

    Delete Train.java

--- Train.java
+++
@@ -1,7 +0,0 @@
-public class Train extends LandTransport{
-    protected String name;
-    public Train(String name, String mode, double price, boolean isRental, boolean isTicket, String pickup, int maxPassengers, double avgSpeed) {
-        super(mode, price, isRental, isTicket, pickup, maxPassengers, avgSpeed);
-        this.name = name;
-    }
-}
