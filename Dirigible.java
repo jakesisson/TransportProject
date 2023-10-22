dbe2888902739ebab4ec8396306a4c1da744283e
jakesisson <139182470+jakesisson@users.noreply.github.com>
2023-10-22 12:25:50 -0400

    Delete Dirigible.java

--- Dirigible.java
+++
@@ -1,7 +0,0 @@
-public class Dirigible extends AirTransport{
-    
-    public Dirigible(String name, String mode, double price, boolean isRental, boolean isTicket, String pickup, int maxPassengers, double avgSpeed) {
-        super(isRecreational, isLongDistance, name, mode, price, isRental, isTicket, pickup, maxPassengers, avgSpeed);
-        
-    }
-}
