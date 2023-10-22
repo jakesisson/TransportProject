bec2ce2e1f2697cc0891f6946b38caea4fe315aa
jakesisson <139182470+jakesisson@users.noreply.github.com>
2023-10-22 12:24:43 -0400

    Delete AirTransport.java

--- AirTransport.java
+++
@@ -1,9 +0,0 @@
-public class AirTransport extends Transport{
-    protected boolean isRecreational;
-    protected boolean isLongDistance;
-    public AirTransport(boolean isRecreational, boolean isLongDistance, String name, String mode, double price, boolean isRental, boolean isTicket, String pickup, int maxPassengers, double avgSpeed) {
-        super(name, mode, price, isRental, isTicket, pickup, maxPassengers, avgSpeed);
-        this.isRecreational = isRecreational;
-        this.isLongDistance = isLongDistance;
-    }
-}
